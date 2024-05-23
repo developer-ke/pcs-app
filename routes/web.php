03bfa109cac8f849cf34d38f18e697f11817dd81
developer-ke <antonytorotich@gmail.com>
2024-05-23 11:31:15 +0300

    added cache clear routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Auth\ProviderController;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/')->middleware(['auth', 'checkAuthenticated']);
 Auth::routes();
@@ -11,3 +12,10 @@ Route::get('/home')->name('home')->middleware(['auth', 'checkAuthenticated']);
 // third paty authentications with laravel socialite
 Route::get('/auth/{provider}/redirect', [ProviderController::class, 'redirect'])->name('auth.redirect');
 Route::get('/auth/{provider}/callback', [ProviderController::class, 'callback'])->name('auth.callback');
+
+Route::get('/clear-cache', function () {
+    Artisan::call('cache:clear');
+    Artisan::call('route:clear');
+    Artisan::call('config:clear');
+    echo 'cache clear succesfully';
+});
